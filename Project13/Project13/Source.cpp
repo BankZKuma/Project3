091abc7383d81465e84a724981d0e50a5bd2778a
BankZKuma <69130573+BankZKuma@users.noreply.github.com>
2020-08-14 17:58:57 +0700

    Update Source.cpp

--- Project13/Project13/Source.cpp
+++ Project13/Project13/Source.cpp
@@ -1,47 +1,53 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-int main()
+int x[1000][1000], y[1000][1000], ans[1000][1000];
+int row, col;
+int num(int Q)
 {
-	int sum;
-	int row, col;
 	int i, j;
-	int x[100][100], y[100][100];
-	printf("Enter row and col : ");
-	scanf("%d %d", &row, &col);
-	printf("First Matrix\n");
-	for (i = 0; i < row; i++)
+	for ( i = 0; i < row; i++)
 	{
 		for (j = 0; j < col; j++)
+		{
+			 ans[i][j] = Q == 1 ? x[i][j] + y[i][j] : x[i][j] - y[i][j];
+		}
+	}
+	return ans[i][j];
+}
+int main()
+{
+	int Q;
+	printf(":::::::::You can enter 1 or 0 1equal Addtion 0equal Subtract:::::::::::  ");
+	scanf_s("%d", &Q);
+	printf("Enter row : ");
+	scanf_s("%d", &row);
+	printf("Enter col : ");
+	scanf_s("%d", &col);
+	printf("::::::::::First Matrix :::::::::::\n");
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
 		{
 			scanf_s("%d", &x[i][j]);
 		}
 	}
-	printf("Second Matrix\n");
-	for (i = 0; i < row; i++)
+	printf("::::::::::Second Matrix :::::::::::\n");
+	for (int i = 0; i < row; i++)
 	{
-		for (j = 0; j < col; j++)
+		for (int j = 0; j < col; j++)
 		{
 			scanf_s("%d", &y[i][j]);
 		}
 	}
-	printf("Sum");
-	for (i = 0; i < row; i++)
+	num(Q);
+	printf("::::::::::Result Matrix :::::::::::\n");
+	for (int i = 0; i < row; i++)
 	{
-		for (j = 0; j < col; j++)
+		for (int j = 0; j < col; j++)
 		{
-			sum = y[i][j] + x[i][j];
+			printf("%d", ans[i][j]);
 		}
-		printf("\n");
-	}
-	for (i = 0; i < row; i++)
-	{
-		printf("[");
-		for (j = 0; j < col; j++)
-		{
-			printf("%d\t ", sum);
-		}
-		printf("]");
-		printf("\n");
+		printf("\n\n");
 	}
 	return 0;
 }
